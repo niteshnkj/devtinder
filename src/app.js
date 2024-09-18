eea608ca0a5eb7f18c5dd16e8c6e973be1982dd3
Nitesh Kumar Jha <niteshkrjha.nkj@gmail.com>
2024-09-18 23:42:49 +0530

    basic http methods

--- src/app.js
+++ src/app.js
@@ -1,16 +1,33 @@
 const express = require("express");
 const app = express();
 
-app.use("/",(req, res) => {
-  res.send("hello from server");
+// order of execution matters if this remains here execution will not go to the next route 
+app.use("/user",(req,res)=>{
+    res.send("HAHAHAHAHAHAHAHAHAHA")
+})
+
+app.get("/user", (req, res) => {
+  res.send({ firstname: "Nitesh", lastname: "Jha" });
 });
+app.post("/user", (req, res) => {
+    res.send("User saved sucessfully!");
+});
+app.patch("/user", (req, res) => {
+    res.send("User updated sucessfully ");
+});
+app.delete("/user", (req, res) => {
+    res.send("User deleted sucessfully");
+});
+
 app.use("/hello", (req, res) => {
-  res.send("hello grom server");
+  res.send("hello from server");
 });
 app.use("/test", (req, res) => {
   res.send("testing output for test route");
 });
-
+// app.use("/", (req, res) => {
+//   res.send("hello Nitesh");
+// });
 app.listen(7777, () => {
   console.log("server is running at 7777");
 });
