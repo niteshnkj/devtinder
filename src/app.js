5f7954d9b8b2f4c44d9faa584e1fa59820ebea0c
Nitesh Kumar Jha <niteshkrjha.nkj@gmail.com>
2024-09-19 23:21:57 +0530

    playing with request handlers

--- src/app.js
+++ src/app.js
@@ -1,33 +1,40 @@
 const express = require("express");
 const app = express();
 
-// order of execution matters if this remains here execution will not go to the next route 
-app.use("/user",(req,res)=>{
-    res.send("HAHAHAHAHAHAHAHAHAHA")
-})
+// *? playing with multiple route handlers
 
-app.get("/user", (req, res) => {
-  res.send({ firstname: "Nitesh", lastname: "Jha" });
-});
-app.post("/user", (req, res) => {
-    res.send("User saved sucessfully!");
-});
-app.patch("/user", (req, res) => {
-    res.send("User updated sucessfully ");
-});
-app.delete("/user", (req, res) => {
-    res.send("User deleted sucessfully");
-});
+app.use(
+  "/user",
+  (req, res, next) => {
+    console.log("Request handler 1");
+    // res.send("Response 1");
+    next();
+  },
+  (req, res, next) => {
+    console.log("Request handler 2");
+    // res.send("Response 2");
+    next();
+  }
+  ,
+  (req, res, next) => {
+    console.log("Request handler 3");
+    // res.send("Response 3");
+    next();
+  }
+  ,
+  (req, res, next) => {
+    console.log("Request handler 4");
+    // res.send("Response 4");
+    next();
+  }
+  ,
+  (req, res, next) => {
+    console.log("Request handler 5");
+    res.send("Response 5");
+    next();
+  }
+);
 
-app.use("/hello", (req, res) => {
-  res.send("hello from server");
-});
-app.use("/test", (req, res) => {
-  res.send("testing output for test route");
-});
-// app.use("/", (req, res) => {
-//   res.send("hello Nitesh");
-// });
 app.listen(7777, () => {
   console.log("server is running at 7777");
 });
