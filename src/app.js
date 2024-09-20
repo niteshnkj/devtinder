4e02299f46dfd3bebc5e45d523d10528467c1a3d
Nitesh Kumar Jha <niteshkrjha.nkj@gmail.com>
2024-09-20 19:15:52 +0530

    db conection using mongoose

--- src/app.js
+++ src/app.js
@@ -1,55 +1,15 @@
 const express = require("express");
-const { adminAuth, userAuth } = require("./middlewares/auth");
+const { connectDb } = require("./config/database");
+
 const app = express();
-
-// // ? writting middleware for admin and user
-
-// app.use("/admin", adminAuth);
-
-// app.get("/admin/getallusers", (req, res, next) => {
-//   console.log("All list fetched");
-//   res.send("users list fetched sucesfully");
-// });
-
-// app.get("/admin/deleteuser", (req, res) => {
-//   console.log("User deleted sucessfully");
-//   res.send("user deleted sucessfully");
-// });
-
-// app.post("/user/register", userAuth, (req, res) => {
-//   console.log("user registered");
-//   res.send("user created sucessfully");
-// });
-
-// app.get("/user/login", (req, res) => {
-//   console.log("loggedin user");
-//   res.send("user loggedin sucessfully");
-// });
-
-// ?writting wild card  error middleware
-// order matters a lot
-
-// app.use("/", (err, req, res, next) => {
-//   if (err) {
-//     res.status(401).send("Oops something went wrong.......!!");
-//   }
-// });
-app.get("/userdata", (req, res) => {
-//   try {
-    
-//   } catch (error) {
-//     res.status(500).send("new error");
-//   }
-throw new Error("dvhk");
-    res.send("userdata");
-});
-
-app.use("/", (err, req, res, next) => {
-  if (err) {
-    res.status(401).send("Oops something went wrong.......!!");
-  }
-});
-
-app.listen(7777, () => {
-  console.log("server is running at 7777");
-});
+// database connection
+connectDb()
+  .then(() => {
+    console.log("Database connection established");
+    app.listen(7777, () => {
+      console.log("server is running at 7777");
+    });
+  })
+  .catch((err) => {
+    console.error("database can't be connected" + err);
+  });
