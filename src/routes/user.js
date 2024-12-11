c873b6a11a56ef859474d35564e6c3a496b18c9c
nitesh <niteshkrjha.nkj@gmail.com>
2024-12-11 19:09:54 +0530

    feed api and pagination

--- src/routes/user.js
+++ src/routes/user.js
@@ -63,6 +63,11 @@ userRouter.get("/user/connections", userAuth, async (req, res) => {
 userRouter.get("/feed", userAuth, async (req, res) => {
   try {
     const loggedInUser = req.user;
+    const page = parseInt(req.query.page) || 1;
+    let limit = parseInt(req.query.limit) || 10;
+    
+    limit = limit > 50 ? 50 : limit;
+    const skip = (page - 1) * limit;
     //find all the connection request that are present in logged in user
     const connectionRequest = await ConnectionRequest.find({
       $or: [{ fromUserId: loggedInUser }, { toUserId: loggedInUser }],
@@ -79,7 +84,10 @@ userRouter.get("/feed", userAuth, async (req, res) => {
         { _id: { $nin: Array.from(hideUserData) } },
         { _id: { $ne: loggedInUser._id } },
       ],
-    }).select(SAFE_USER_DATA);
+    })
+      .select(SAFE_USER_DATA)
+      .skip(skip)
+      .limit(limit);
     res.json({ data: users });
   } catch (error) {
     res.send(error.message);
